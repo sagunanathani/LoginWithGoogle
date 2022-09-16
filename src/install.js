f5fc932c22eb12761b87044a7ad8ba58b4fd438a
sagunanathani <sagunakathiriya.bvminfotech@gmail.com>
2022-09-15 18:10:56 -0700

    feat:login with google

---
+++ src/install.js
@@ -0,0 +1,10 @@
+// Google Cloud Console -> add credentials then
+
+// First, gapi-script is an npm package that loads Google API scripts and initializes some functions.
+
+// The second package is called react-google-login and allows us to integrate the Google login feature into our React app. Additionally, react-google-login allows us to obtain the access tokens we need to access all Google APIs both quickly and safely.
+
+// Before we install and begin integrating gapi-script and react-google-login into our React app, we will have to first configure a few things with Google. Let’s start by getting a Google client ID.
+
+// npm install react-google-login gapi-script
+// yarn add react-google-login
